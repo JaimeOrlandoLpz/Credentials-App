bffa88dd3487d332d59e91b1e16e2c140543f83b
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-12-03 10:33:14 -0600

    useEffect() to verify validity of the form whenever dependencies change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
+
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+
+  // Evaluates After every aspect of the component has been evaluated and only if the dependencies have changed
+  useEffect(() => {
+    const storeUserLoggedInformation = localStorage.getItem('isLoggedIn'); // Check if Item is stored in local storage
+    if(storeUserLoggedInformation === '1'){
+      setIsLoggedIn(true); // If local storage has inforrmation of a previous log, we sign the user in automatically without requiring credenteals
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // We save a key-value pair in the local storage when the user logs in
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
